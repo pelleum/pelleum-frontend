869392884c5f9c8458537c3643f3db70d99e4645
ernesto1034 <ernesto1034@yahoo.com>
2021-11-06 13:53:00 -0600

    installed expo-secure, completed Education and Blog Screens (hardcoded list with parent-child navigation capaability)

--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { VStack, NativeBaseProvider } from "native-base"
 
-const BlogScreen = () => {
+const BlogScreen = props => {
+    const dataReceived = props.navigation.state.params;
+    //console.log(dataReceived);
+    
     return (
-        <View>
-            <Text>Blog Screen</Text>
+        <View style={styles.mainContainer}>
+            <ScrollView>
+                <NativeBaseProvider>
+                    <VStack>
+                        <Image source={dataReceived.imageSource} resizeMode={'contain'} />
+                        <Text style={styles.dateText}>{dataReceived.blogDate}</Text>
+                        <Text style={styles.titleText}>{dataReceived.blogTitle}</Text>
+                        <Text style={styles.blogText}>{dataReceived.blogContent}</Text>
+                    </VStack>
+                </NativeBaseProvider>
+            </ScrollView>
         </View>
     );
 };
 
 export default BlogScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    dateText: {
+        fontSize: 15,
+        marginVertical: 5,
+        paddingHorizontal: 5
+    },
+    titleText: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginVertical: 5,
+        paddingHorizontal: 5
+    },
+    blogText: {
+        fontSize: 16,
+        marginVertical: 5,
+        paddingHorizontal: 5
+    },
+});
